dedff05d1eb121b1d5a17a8dad1b587ccc3267f4
xuml <xuml@abccapital.cn>
2019-09-24 14:43:28 +0800

    新建动态规划模块

---
+++ dynamic-programming-demo/src/main/java/io/github/feiyizhan/DynamicProgrammingDemoApplication.java
@@ -0,0 +1,13 @@
+package io.github.feiyizhan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicProgrammingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicProgrammingDemoApplication.class, args);
+	}
+
+}
