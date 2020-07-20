80a4582360b5e7146891d71b499c79e12fbd7f6f
xuml <xuml@abccapital.cn>
2020-07-20 16:23:56 +0800

    初始化java-lang-demo版本

---
+++ java-lang-demo/src/test/java/io/github/feiyizhan/JavaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.feiyizhan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
